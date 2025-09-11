839d53d013ee348bdd2caf7216780ab6b52ffe3a
Austin McDonald <austinmcdonald2011@gmail.com>
2025-09-11 11:07:37 -0400

    Week 4 #2

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -60,22 +60,40 @@ import turtle
 def can_be_triangle(a, b, c):
     if a + b > c and a + c > b and b + c > a:
         print(a, " ", b, " ", c, " can form a triangle.")
+        import math
+        turtle.speed(1)
+        turtle.color("white")
+        turtle.bgcolor("black")
+        turtle.pensize(3)
+        turtle.pendown()
+        scale = 20
+        # Law of Cosines to find angles
+        # Angle at A (between sides b and c)
+        angle_A = math.degrees(math.acos((b**2 + c**2 - a**2) / (2*b*c)))
+        # Angle at B (between sides a and c)
+        angle_B = math.degrees(math.acos((a**2 + c**2 - b**2) / (2*a*c)))
+        # Angle at C (between sides a and b)
+        angle_C = math.degrees(math.acos((a**2 + b**2 - c**2) / (2*a*b)))
+        # Draw triangle
+        turtle.forward(a*scale)
+        turtle.left(180 - angle_C)
+        turtle.forward(b*scale)
+        turtle.left(180 - angle_A)
+        turtle.forward(c*scale)
+        turtle.left(180 - angle_B)
+        turtle.exitonclick()
     else:
         print(a, " ", b, " ", c, " cannot form a triangle.")
-    turtle.speed(1)
-    turtle.color("white")
-    turtle.bgcolor("black")
-    turtle.pensize(3)
-    turtle.pendown()
-    turtle.forward(a)
-    turtle.left(120)
-    turtle.forward(b)
-    turtle.left(120)
-    turtle.forward(c)
-    turtle.left(120)
-    turtle.done()
 
-can_be_triangle(3, 4, 5)
+
+# Get user input for triangle sides
+try:
+    a = float(input("Enter length of side a: "))
+    b = float(input("Enter length of side b: "))
+    c = float(input("Enter length of side c: "))
+    can_be_triangle(a, b, c)
+except ValueError:
+    print("Please enter valid numbers for the sides.")
 
 
 print("********** Ch 5 Exercise 4 **********")
