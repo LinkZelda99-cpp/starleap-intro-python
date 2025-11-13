11a3bc3f8c2f8ed8d50123eadb11ef94cf12cc13
Austin McDonald <austinmcdonald2011@gmail.com>
2025-11-13 09:53:45 -0500

    Did Pig Latin

--- chapters/10_2_homework/pig_latin.py
+++ chapters/10_2_homework/pig_latin.py
@@ -6,11 +6,37 @@
 # word_to_pig_latin: Converts the input string to pig latin and returns the pig latin string
 # Input: one string - the word in English
 # Return: one string - the word in Pig Latin
-def word_to_pig_latin(english):
+def word_to_pig_latin(word):
+    """Convert a single English word to Pig Latin and return it."""
+    if not word:
+        return ''
+    w = word.lower()
+    vowels = 'aeiou'
+    for letter in w:
+        if letter not in vowels:
+            vowels = 'aeiouy'
+        else:
+            vowels = 'aeiou'
+            break
+    
+    for i, ch in enumerate(w):
+        if ch in vowels:
+            if i == 0:
+                return w + 'hay'
+            return w[i:] + w[:i] + 'ay'
+    return w + 'ay'
 
-    # Write your function here
-    return ''
+def sentence_to_pig_latin():
+    """Convert a sentence of English words to Pig Latin and return it."""
+    sentence = input("Enter a sentence in English: ")
+    words = sentence.split()
+    pig_latin_words = [word_to_pig_latin(word) for word in words]
+    pig_latin_words = ' '.join(pig_latin_words)
+    pig_latin_words = pig_latin_words.capitalize()
+    append_punctuation = sentence[-1] if sentence[-1] in '.!?' else ''
+    return pig_latin_words + append_punctuation
 
+print(sentence_to_pig_latin())
 
 def test_translator():
     pairs = [
@@ -23,11 +49,11 @@ def test_translator():
     for pair in pairs:
         english = pair[0]
         answer = pair[1]
-        pl = word_to_pig_latin(english)
+        pl = word_to_pig_latin()
         if (pl != answer):
             print(f"Oops!  {english} should translate to {answer}, not {pl} !")
             errors += 1
     if errors == 0:
         print('Congrats!  Your translator is working well.')
 
-test_translator()
+#test_translator()
