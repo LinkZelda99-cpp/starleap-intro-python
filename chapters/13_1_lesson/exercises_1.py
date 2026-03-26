81731ef16c9b5ac77361b7121cd0f050d7563a4e
Austin McDonald <austinmcdonald2011@gmail.com>
2026-03-26 09:50:55 -0400

    Did ch 14 exercises 1-4

--- chapters/13_1_lesson/exercises_1.py
+++ chapters/13_1_lesson/exercises_1.py
@@ -1,61 +1,69 @@
-import random
-def sort(num_list):
-    for i in range(len(num)-1):
-        print(num[i], num[i+1])
-        if num[i]>num[i+1]:
-            c = num[i]
-            num[i] = num[i+1]
-            num[i+1] = c
-        print(i)
-        print(num)
-num = [11,45,65,23,63,24]
-number = 0
-# while number < 4:
-#     sort(num)
-#     number += 1
 
+print("********** Ch 13 Exercise 1, 2, and 3**********")
+import random
 import string
 from operator import itemgetter
-word_list = ['']
+
+def sort(num):
+    for j in range(len(num) - 1):
+        for i in range(len(num) - 1 - j):
+            if num[i] > num[i + 1]:
+                num[i], num[i + 1] = num[i + 1], num[i]
+
+def random_numbers(n):
+    nums = []
+    for i in range(n):
+        nums.append(random.randint(1, 10000))
+    return nums
+
 def moby_remove_whitespaces_punctuation(fin):
-    
-    count = 0
-    d = dict()
+    import re
+    d = {}
     for line in fin:
-        for c in string.punctuation:
-            line.replace(c, " ")
-        word = line.strip().lower()
-        # word = word.translate(str.maketrans('', '', string.punctuation))
-        word = word.replace(string.punctuation, "")
-        line.strip
-        line = line.replace("\n", "") 
-        line = line.replace("  ", " ")
-        words = line.split(" ")
-    
+        line = line.lower()
+        line = re.sub(r'[^a-zA-Z\s]', ' ', line)
+        words = line.split()
         for w in words:
-          if w not in d:
-             d[w] = 1
-          else:
-             d[w] += 1
-        # print(d)
+            if w:
+                if w not in d:
+                    d[w] = 1
+                else:
+                    d[w] += 1
     return d
-      #  for w in words:
-       #     word_list.append(w)
-        #    count = count + 1
-     #   print(f"{word} ")
-    
-    #return count
-    
-# count = moby_remove_whitespaces_punctuation(open('shared\Moby_Dick.txt'))
-#print(f"Word list: {word_list}")
-#
+
 f_name = 'shared/Moby_Dick.txt'
-d = moby_remove_whitespaces_punctuation(open(f_name))
-# print(f"Count: {count}")
-# print(string.punctuation)
-# for k, v in d.items():
-    # print(f"{k} {v}")
-# sv = dict()
-# sv = dict(sorted(d.items(), key=itemgetter(1)))
-sv = dict(int(sort(d)))
-print(sv)
+word_list = 'shared/words.txt'
+
+with open(f_name, 'r', encoding='utf-8') as f:
+    d = moby_remove_whitespaces_punctuation(f)
+
+sorted_words = sorted(d.items(), key=itemgetter(1), reverse=True)
+
+print("Top 20 most common words:")
+for word, count in sorted_words[:20]:
+    print(word, count)
+    
+print("********** Ch 13 Exercise 4 **********")
+
+def not_in():
+    with open(f_name, 'r', encoding='utf-8') as f:
+        d = moby_remove_whitespaces_punctuation(f)
+
+    with open(word_list, 'r', encoding='utf-8') as f:
+        word_set = set()
+        for line in f:
+            word_set.add(line.strip().lower())
+
+    not_in_list = []
+
+    for w in d:
+        if w not in word_set:
+            not_in_list.append(w)
+
+    print(not_in_list)
+
+not_in()
+    
+print("********** Ch 12 Exercise 5 **********")
+import random
+
